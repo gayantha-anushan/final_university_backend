5072e50222056e2ac5024638a01cdbb83c9267bb
geoneth tricks <geontricks@gmail.com>
2022-02-04 14:47:12 +0530

    connecting frontend

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(cors())
 app.use(express.static(__dirname+'/public'))
 app.use(express.static(__dirname+'/images'))
 
-//const mongo_url = "mongodb://localhost:27017/application_final";
+//const mongo_url = "mongodb://localhost:27017/application_final"
+
 const mongo_url = "mongodb+srv://anushan:gaya.kas123@cluster0.5dn9k.mongodb.net/agrosupport?retryWrites=true&w=majority";
 
 mongoose.connect(mongo_url,{
