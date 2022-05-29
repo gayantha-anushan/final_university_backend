3e5a4d81a26346cf3ae72c3eb7af038f79913e24
Kasun Thenuwara <iamkasun97@gmail.com>
2022-05-29 17:59:17 +0530

    cart route and schema

--- models/cart.js
+++ models/cart.js
@@ -31,3 +31,15 @@ const cartSchema = new Schema({
 });
 
 module.exports = mongoose.model("Cart",cartSchema);
+
+/**
+
+    {
+        "postId" : "628a85e502d8bd0016651328",
+        "buyerId" : "628a847702d8bd0016651327",
+        "sellerId" : "628b1e0aed7d940016506aa6",
+        "price" : 100,
+        "qty" : 100
+    }
+
+ */
--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,42 @@
 var express = require('express');
 var router = express.Router();
 const bodyParser = require('body-parser');
+const Cart = require('../models/cart');
+
 
 router.use(bodyParser.json());
 
-router.post('/new-cart-item' , async (req , res , next) => {
-    console.log(req.body.cartItem.json);
+router.get('/' , async (req, res, next) => {
+    var cart = await Cart.find({}).populate('buyerId').populate('sellerId');
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(cart);
+})
+
+router.post('/addtocart' , async (req , res , next) => {
+   var cartItem = new Cart({
+       postId : req.body.postId,
+       buyerId : req.body.buyerId,
+       sellerId : req.body.sellerId,
+       price : req.body.price,
+       qty : req.body.qty
+   });
+
+   cartItem.save()
+   .then(cartItem => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(cartItem);
+   } , err => {
+       next(err);
+   })
+});
+
+router.post('/removecartitem/:cartItemId' , async (req, res ,next) => {
+    var cartItem = await Cart.findByIdAndRemove({_id : req.params.cartItemId});
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(cartItem);
 });
 
 module.exports = router;
