790478b77185dffbf34ffcdea7f8a1e31a31be70
geoneth tricks <geontricks@gmail.com>
2022-09-02 15:43:43 +0530

    success

--- routes/Posts.js
+++ routes/Posts.js
@@ -11,13 +11,45 @@ const mongoose = require('mongoose');
 
 router1.use(bodyParser.json());
 
-router1.get("/" ,async (req , res , next) => {
+router1.get("/" ,(req , res , next) => {
     //{$where:"this.quantity > this.successQuantity"}
-    var result = await Post.find({}).populate("author");
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.json(result);
+    Post.find({}).populate("author").then((result) => {
+        return result.filter(item => {
+            if (item.quantity > item.successQuantity) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+    }).then((nres) => {
+        res.status(200).send(nres);
+    }).catch((error) => {
+        console.log(error)
+        res.status(500).send()
+    })
+    //res.statusCode = 200;
+    //res.setHeader("Content-Type", "application/json");
+    //res.json(result);
 });
+
+router1.post("/update-post", (req, res) => {
+    console.log(req.body);
+    Post.findOne({ _id: req.body.id }).then((post_res) => {
+        post_res.title = req.body.title;
+        post_res.quantity = req.body.quantity;
+        post_res.price.wholeseller = req.body.wholeseller;
+        post_res.price.localseller = req.body.localseller;
+        post_res.price.customer = req.body.customer;
+        post_res.description = req.body.description;
+        return post_res.save();
+    }).then((resu) => {
+        console.log(resu)
+        res.status(200).send()
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send();
+    })
+})
 
 router1.get("/singlepost/:id", (req, res) => {
     Post.find({ _id: req.params.id }).populate("author").then((result) => {
@@ -115,9 +147,13 @@ router1.put("/updatepost/:postId",async (req, res , next) => {
 //     })
 // })
 
-router1.post("/deletepost/:postId", async (req, res , next) => {
-    const post = await Post.deleteOne({_id : req.params.postId});
-    res.json(post);
+router1.post("/deletepost/:postId",(req, res , next) => {
+    Post.deleteOne({ _id: req.params.postId }).then((resu) => {
+        res.status(200).send(resu);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send();
+    })
 });
 
 
