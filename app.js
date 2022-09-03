88dcc3ca3067f4197ad39bbf19c33ed365eb53ba
geoneth tricks <geontricks@gmail.com>
2022-09-03 14:15:53 +0530

    ok

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/post-img", express.static(path.join(__dirname + '/routes/uploads')))
 app.use('/profile',express.static(path.join(__dirname + '/routes/profiles')))
 
-const mongo_url = "mongodb+srv://gayantha:gaya.1234@cluster0.7a6zy.mongodb.net/vege-sup-test-ppp?retryWrites=true&w=majority";
-//const mongo_url = "mongodb+srv://gayantha:gaya.1234@cluster0.7a6zy.mongodb.net/vege-sup-x?retryWrites=true&w=majority";
+//const mongo_url = "mongodb+srv://gayantha:gaya.1234@cluster0.7a6zy.mongodb.net/vege-sup-test-ppp?retryWrites=true&w=majority";
+const mongo_url = "mongodb+srv://gayantha:gaya.1234@cluster0.7a6zy.mongodb.net/vege-sup?retryWrites=true&w=majority";
 
 mongoose.connect(mongo_url,
   {
